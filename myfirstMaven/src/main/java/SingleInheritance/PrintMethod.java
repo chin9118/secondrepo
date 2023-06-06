0dd25d0b971f46860442d4883c840f60b278da46
Jishnu <Jishnu@LAPTOP-VBIL4K2H>
2023-06-07 08:46:43 +1000

    first commit

---
+++ myfirstMaven/src/main/java/SingleInheritance/PrintMethod.java
@@ -0,0 +1,11 @@
+package SingleInheritance;
+
+public class PrintMethod {
+	void print()
+	{
+
+System.out.println("Hello world");
+	
+
+}
+}
---
+++ myfirstMaven/src/main/java/SingleInheritance/PrintShow.java
@@ -0,0 +1,12 @@
+package SingleInheritance;
+
+public class PrintShow extends PrintMethod{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintShow obj=new PrintShow();
+		obj.print();
+
+	}
+
+}
